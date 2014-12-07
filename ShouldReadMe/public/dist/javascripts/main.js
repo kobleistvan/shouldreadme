6e1d45997f125ddfe591319dc996a0b16e09063d
Koble Istvan <kobleistvan@gmail.com>
2014-12-07 15:21:00 +0000

    grunt

--- ShouldReadMe/public/dist/javascripts/main.js
+++ ShouldReadMe/public/dist/javascripts/main.js
@@ -29,6 +29,37 @@ $(document).ready(function () {
     'use strict';
     SRM.init();
 });
+/*global SRM, Backbone, JST */
+
+SRM.Views = SRM.Views || {};
+
+(function () {
+    'use strict';
+
+    SRM.Views.Dashboard = Backbone.View.extend({
+        template: JST['public/javascripts/templates/dashboard/baseDashboard.hbs'],
+        el: '#main',
+        events: {     
+            },
+
+        initialize: function (options) {
+            this.options = options;
+        },
+
+        templateData: function() {
+            return {};
+        },
+
+        render: function () {
+            this.renderTemplate(this.templateData());
+
+            return this;
+        }
+
+    });
+
+})();
+
 /*global SRM, Backbone*/
 
 SRM.Routers = SRM.Routers || {};
