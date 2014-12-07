7be167f3a26be0244a9c0b82b2b9273f15cc6f42
Koble Istvan <kobleistvan@gmail.com>
2014-12-07 21:09:21 +0000

    grunt

--- ShouldReadMe/public/dist/javascripts/main.js
+++ ShouldReadMe/public/dist/javascripts/main.js
@@ -132,6 +132,76 @@ SRM.Views = SRM.Views || {};
 (function () {
     'use strict';
 
+    SRM.Views.EditPanel = Backbone.View.extend({
+        template: JST['public/javascripts/templates/dashboard/editPanelComponents/editPanel.hbs'],
+        el: '.preview-section',
+        events: {
+        },
+
+        initialize: function (options) {
+            this.options = options;
+        },
+
+        templateData: function() {
+            return {}       
+        },
+
+        render: function () {
+            this.renderTemplate(this.templateData());
+            return this;
+        },
+    });
+
+})();
+
+/*global SRM, Backbone, JST */
+
+SRM.Views = SRM.Views || {};
+
+(function () {
+    'use strict';
+
+    SRM.Views.SidebarTips = Backbone.View.extend({
+        template: JST['public/javascripts/templates/dashboard/tips.hbs'],
+        el: '.tips-section',
+        events: {
+        },
+        defaults : {
+            content : 'Add icons and create your own fineprint!'
+        },
+
+        initialize: function (options) {
+            this.options = options;
+        },
+
+        templateData: function() {
+            var content;
+            if(this.options && this.options.content){
+                content = this.options.content;
+            }
+            else{
+                content = this.defaults.content;
+            }
+            
+            return {content : content}       
+        },
+
+        render: function () {
+            this.renderTemplate(this.templateData());
+            return this;
+        },
+        
+    });
+
+})();
+
+/*global SRM, Backbone, JST */
+
+SRM.Views = SRM.Views || {};
+
+(function () {
+    'use strict';
+
     SRM.Views.SidebarIcons = Backbone.View.extend({
         template: JST['public/javascripts/templates/dashboard/icons.hbs'],
         el: '.icons-subsection',
